8286af725410900c3907026969b3ba55295f9090
amardeepbhullar <amardeepsinghbhullar@hotmail.com>
2023-01-23 17:10:39 -0800

    Initial Files

--- addnum.py
+++ addnum.py
@@ -1,5 +1,5 @@
 #this code is to add two numbers
-a = 2
-b = 5
-c = a + b
-print(c)
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+sum = float(num1) + float(num2)
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
